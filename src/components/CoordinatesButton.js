d025ec5378d3d11b6997be500058496a17c3b250
vuonga1103 <vuonga1103@yahoo.com>
2020-08-25 00:32:16 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,14 @@
-// Code CoordinatesButton Component Here
+import React from 'react';
+
+const CoordinatesButton = (props) => {
+
+  const {onReceiveCoordinates} = props;
+
+  const handleClick = e => onReceiveCoordinates([e.clientX, e.clientY]);
+
+    // Within CoordinatesButton, render a button. On click of the button, create an array with two elements: the X and Y coordinates of the mouse. Find these using the event data.
+  return <button onClick={handleClick}>Coordinates</button>
+}
+
+
+export default CoordinatesButton;
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,20 @@
-// Code DelayedButton Component Here
+import React from 'react';
+import { verifyAndRestore } from 'sinon';
+
+const onDelayedClick = (props) => {
+  const {onDelayedClick, delay} = props;
+
+  // If you want to access the event properties in an asynchronous way, you should call event.persist() on the event, which will remove the synthetic event from the pool and allow references to the event to be retained by user code.
+
+  const handleClick = e => {
+    e.persist();
+    window.setTimeout(() => {
+      onDelayedClick(e)
+    }, delay)
+  }
+
+  // Create a button that, when clicked, will pass the click event to the onDelayedClick prop within a setTimeout(). The setTimeout() should be set to this.props.delay.
+  return <button onClick={handleClick}>Click Me</button>
+}
+
+export default onDelayedClick;
